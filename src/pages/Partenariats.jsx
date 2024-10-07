97f49a066a3cdfbe46a9ba6f77560908037ac020
BellaArthur <145237169+BellaArthur@users.noreply.github.com>
2024-10-07 04:47:47 +0100

    Update Partenariats.jsx

--- src/pages/Partenariats.jsx
+++ src/pages/Partenariats.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Phone, Mail, Facebook, Twitter, Instagram, Youtube, Users, HandCoins } from 'lucide-react'
 import fadec4 from '../assets/img/Fadec4.jpg'
 import minjec from '../assets/img/Minjec.jpg'
@@ -13,7 +14,7 @@ const Partenariats = () => {
         <div className="">
 
             {/* first */}
-            <div className="container  px-4 sm:px-8 pt-10 pb-16 flex flex-col gap-3 md:flex-row bg-green-100">
+            <div className="  px-4 sm:px-8 pt-10 pb-16 flex flex-col gap-3 md:flex-row bg-green-100">
 
                 <div className="flex flex-col">
                     <h3 className='text-base text-brandPrimary font-semibold'>Soutnez-Nous</h3>
@@ -44,7 +45,7 @@ const Partenariats = () => {
             {/*end of first*/}
 
             {/* second */}
-            <div className="container px-4 sm:px-8 pt-12 pb-16 flex flex-col gap-5 md:grid md:grid-cols-2  ">
+            <div className=" px-4 sm:px-8 pt-12 pb-16 flex flex-col gap-5 md:grid md:grid-cols-2  ">
 
                 <div className="flex flex-col gap-3 items-center md:grid md:grid-cols-2 lg:grid lg:grid-cols-3">
                     <img className='shadow-lg' src={unfpa} alt="" />
@@ -61,7 +62,7 @@ const Partenariats = () => {
             {/*end of second */}
 
             {/*third*/}
-            <div className="container pt-12 pb-16 px-4 sm:px-8 flex flex-col md:grid md:grid-cols-2 md:gap-2 bg-green-100 ">
+            <div className=" pt-12 pb-16 px-4 sm:px-8 flex flex-col md:grid md:grid-cols-2 md:gap-2 bg-green-100 ">
 
                 <div className="flex flex-col mb-6 ">
                     <h3 className='text-left text-xl font-bold text-brandPrimary'>Devenez un <span className='text-brandSecondary'>Parternaire</span></h3>
@@ -77,7 +78,7 @@ const Partenariats = () => {
                             <h4 className='text-left text-lg font-bold text-brandPrimary'>Devenez un <span className='text-brandSecondary'>Volontire</span></h4>
                             <p className='text-base text-gray-500'>Ensemble agissons pour le dévelopement dans votre communauté.</p>
                         </div>
-                        <button className='px-3 py-2 w-2/5 md:w-full bg-brandPrimary rounded-lg text-sm font-bold text-white'><a href="/partenariats">Cliquez Ici</a></button>
+                        <button className='px-3 py-2 w-2/5 md:w-full bg-brandPrimary rounded-lg text-sm font-bold text-white'><Link to="/partenariats">Cliquez Ici</Link></button>
                     </div>
                     {/*end of cart*/}
 
@@ -88,7 +89,7 @@ const Partenariats = () => {
                             <h4 className='text-left text-lg font-bold text-brandPrimary'>Devenez un <span className='text-brandSecondary'>Donateur</span></h4>
                             <p className='text-base text-gray-500'>Nous menons des initiatives de fundraising pour financer nos activités.</p>
                         </div>
-                        <button className='px-3 py-2 w-2/5 md:w-full bg-brandSecondary rounded-lg text-sm font-bold text-white'><a href="/partenariats">Cliquez Ici</a></button>
+                        <button className='px-3 py-2 w-2/5 md:w-full bg-brandSecondary rounded-lg text-sm font-bold text-white'><Link to="/partenariats">Cliquez Ici</Link></button>
                     </div>
                     {/*end of cart*/}
                 </div>
